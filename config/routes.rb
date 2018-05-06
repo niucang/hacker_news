7b3e530bda12940f7c0622a7570f88bfc46b9251
chang.liu <chang.liu@alo7.com>
2018-05-06 23:16:24 +0800

    first channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  get 'rooms/show'
+
   root :to => 'news#index'
   resources :comments do
     collection do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417163843) do
+ActiveRecord::Schema.define(version: 20180506140642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20180417163843) do
     t.datetime "updated_at", null: false
     t.index ["new_id"], name: "index_comments_on_new_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
   create_table "news", force: :cascade do |t|
